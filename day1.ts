60c776ccdaa9d6798501e5abfd0397f4ef971237
kps <dev.kps88@gmail.com>
2025-04-17 10:34:49 +0900

    day1 commit

---
+++ day1.ts
@@ -0,0 +1,22 @@
+let a: string = "hello";
+let b: number = 5;
+let c: boolean = true;
+let d: any = "hello";
+let e: unknown = "hello";
+let f: object = { name: "John" };
+let g: null = null;
+let h: undefined = undefined;
+let i: void = undefined;
+let j: never = (() => { throw new Error("never") })();
+let k: bigint = BigInt(12345678901234567890);
+let l: symbol = Symbol("symbol");
+let m: string[] = ["hello", "world"];
+let n: number[] = [1, 2, 3];
+let o: boolean[] = [true, false, true];
+let p: any[] = [1, "hello", true, { name: "John" }];
+let q: [string, number] = ["hello", 5];
+let r: [string, number][] = [["hello", 5], ["world", 10]];
+let s: [string, number] = ["hello", 5];
+let t: [string, number?] = ["hello"]; // optional second element
+let u: [string, ...number[]] = ["hello", 1, 2, 3]; // rest element
+let v: [string, ...number[]] = ["hello"]; // rest element with empty array
