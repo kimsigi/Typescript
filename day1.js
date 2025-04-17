e8b2136992209a2321f461c5ce3eb24da69daf65
kps <dev.kps88@gmail.com>
2025-04-17 15:10:59 +0900

    기본설정 완료

---
+++ day1.js
@@ -0,0 +1,24 @@
+"use strict";
+let a = "hello";
+let b = 5;
+let c = true;
+let d = "hello";
+let e = "hello";
+let f = { name: "John" };
+let g = null;
+let h = undefined;
+let i = undefined;
+//let j: never = (() => { throw new Error("never") })();
+//let k: bigint = BigInt("12345678901234567890"); // 문자열로
+//let l: symbol = Symbol("symbol");
+let m = ["hello", "world"];
+let n = [1, 2, 3];
+let o = [true, false, true];
+let p = [1, "hello", true, { name: "John" }];
+let q = ["hello", 5];
+let r = [["hello", 5], ["world", 10]];
+let s = ["hello"]; // ?는 생략가능하므로 생략
+let t = ["hello", 1, 2, 3]; // ...(rest) 이므로 배열로 들어감
+let u = ["hello"]; // rest element with empty array
+u.push('world'); // string
+console.log(u);
--- day1.ts
+++ day1.ts
@@ -7,16 +7,19 @@ let f: object = { name: "John" };
 let g: null = null;
 let h: undefined = undefined;
 let i: void = undefined;
-let j: never = (() => { throw new Error("never") })();
-let k: bigint = BigInt(12345678901234567890);
-let l: symbol = Symbol("symbol");
+//let j: never = (() => { throw new Error("never") })();
+//let k: bigint = BigInt("12345678901234567890"); // 문자열로
+//let l: symbol = Symbol("symbol");
 let m: string[] = ["hello", "world"];
 let n: number[] = [1, 2, 3];
 let o: boolean[] = [true, false, true];
 let p: any[] = [1, "hello", true, { name: "John" }];
 let q: [string, number] = ["hello", 5];
 let r: [string, number][] = [["hello", 5], ["world", 10]];
-let s: [string, number] = ["hello", 5];
-let t: [string, number?] = ["hello"]; // optional second element
-let u: [string, ...number[]] = ["hello", 1, 2, 3]; // rest element
-let v: [string, ...number[]] = ["hello"]; // rest element with empty array
+let s: [string, number?] = ["hello"]; // ?는 생략가능하므로 생략
+let t: [string, ...number[]] = ["hello", 1, 2, 3]; // ...(rest) 이므로 배열로 들어감
+let u: [string, ...number[]] = ["hello"]; // rest element with empty array
+
+console.log(u);
+u = ['hello-world',1,2,3,4,5];
+console.log(u);
