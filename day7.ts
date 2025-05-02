fa53dbb9f27e01c85de0d364ab4815b1c4e95272
kps <dev.kps88@gmail.com>
2025-05-02 10:12:04 +0900

    day7 commit

--- day7.ts
+++ day7.ts
@@ -1,1 +1,7 @@
-#### 여기서부터는 고급 타입스크립트 내용 ####
+//#### 여기서부터는 고급 타입스크립트 내용 ####
+
+type IsNumber<T> = T extends number ? '숫자' : '비숫자';
+
+type A = IsNumber<100>;       // '숫자'
+type B = IsNumber<'hello'>;   // '비숫자'
+type C = IsNumber<number[]>;  // '비숫자'
